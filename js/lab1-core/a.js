237cbe0a3d8dffb965d03a5601d32087b115d043
kkang-daum <123041788+kkang-daum@users.noreply.github.com>
2024-07-17 17:04:13 +0900

    Add files via upload

---
+++ js/lab1-core/a.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function (e) {
+    //dom node 획득.. 
+    let selectNode = document.querySelector('#a1')
+    selectNode.innerHTML = "helloo"
+    selectNode.style.backgroundColor = 'red'
+})
