e9bad8ecd641d464766b94474f04079a943a7842
kkang-daum <123041788+kkang-daum@users.noreply.github.com>
2024-07-24 08:43:38 +0900

    Add files via upload

---
+++ react/lab3/lab3-app/src/lab1-style/Lab1App.jsx
@@ -0,0 +1,10 @@
+const CSSTest = () => {
+    return (
+        <div className="container">
+            <h2>Global CSS</h2>
+            <button type="button" className="btn btn-primary">Primary</button>
+        </div>
+    )
+}
+
+export default CSSTest
---
+++ react/lab3/lab3-app/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+//앱 내에서 global 하게 적용되어야 하는 css 는 
+//js 에서 import, 한번만 import 하면 앱 전역에서 사용
+//main.jsx 에서 import
+import 'bootstrap/dist/css/bootstrap.css'
+
+import CSSTest from './lab1-style/Lab1App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <CSSTest />
+  </React.StrictMode>,
+)
